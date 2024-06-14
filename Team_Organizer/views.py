38fa86c3255851107f248c5c8309cd43d81084d6
LiedsonSales <liedson.r.sales@gmail.com>
2024-06-13 21:48:50 -0300

    I created templates with pages and partials, and configure static files for the main page css archive (base_static)

--- Team_Organizer/views.py
+++ Team_Organizer/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'Team_Organizer/pages/home.html', context={
+        'name':'liedson'
+    })
